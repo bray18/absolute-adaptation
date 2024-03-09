2880d976a5cf44a2c42b9cf961687aaf9058aef1
Brady Walters <49881821+bray18@users.noreply.github.com>
2024-03-09 16:23:19 -0600

    FUNCTIONAL GAME

--- Assets/Scripts/VideoPlayerScript.cs
+++ Assets/Scripts/VideoPlayerScript.cs
@@ -2,44 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video; // Ensure this namespace is included for video functionality
 
 public class VideoPlayerScript : MonoBehaviour
 {
+    public VideoClip videoClip; // Public variable for the video clip
+
     AudioSource audioSource;
-    // Start is called before the first frame update
-    void EndReached(UnityEngine.Video.VideoPlayer vp) {
-        Debug.Log("End reached");
-        SceneManager.LoadScene("BasicLevel");
     
+    // This method is called when the video reaches its end
+    void EndReached(VideoPlayer vp) {
+        Debug.Log("End reached");
+        SceneManager.LoadScene("BasicLevel"); // Load the scene named "BasicLevel"
     }
+
     void Start()
     {
-
         audioSource = GameObject.FindWithTag("Music").GetComponent<AudioSource>();
-        //Debug.Log(audioSource+" that's the music");
 
+        // Stop the audio using the Singleton pattern
         Singleton.Instance.StopAudio(audioSource);
-        //Debug.Log("Stopped audio");
-        //audioSource = GameObject.FindWithTag("Music").GetComponent<AudioSource>();
-        //Singleton.Instance.StopAudio();
 
+        // Find the Main Camera GameObject
         GameObject camera = GameObject.Find("Main Camera");
-        var videoPlayer = camera.AddComponent<UnityEngine.Video.VideoPlayer>();
-        videoPlayer.clip = tomato anxiety;
+        // Add a VideoPlayer component to the Main Camera
+        var videoPlayer = camera.AddComponent<VideoPlayer>();
 
-        // By default, VideoPlayers added to a camera will use the far plane.
-        // Let's target the near plane instead.
-        videoPlayer.renderMode = UnityEngine.Video.VideoRenderMode.CameraNearPlane;
-        videoPlayer.waitForFirstFrame = true;
-        videoPlayer.loopPointReached += EndReached;
-        videoPlayer.Play();
+        // Assign the public video clip to the video player
+        videoPlayer.clip = videoClip;
 
-        
+        // Configure the video player
+        videoPlayer.renderMode = VideoRenderMode.CameraNearPlane; // Render on the camera's near plane
+        videoPlayer.waitForFirstFrame = true; // Wait for the first frame before starting playback
+        videoPlayer.loopPointReached += EndReached; // Subscribe to the loopPointReached event
+        videoPlayer.Play(); // Start playing the video
     }
 
     // Update is called once per frame
     void Update()
-    {   
-        
+    {
+        // This can be used for any frame-dependent logic
     }
 }
