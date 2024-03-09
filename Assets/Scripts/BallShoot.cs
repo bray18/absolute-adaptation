70ea565dad45d147e797c75c6f13e9c755173f99
Brady Walters <49881821+bray18@users.noreply.github.com>
2024-03-08 19:31:21 -0600

    Basic BallShoot
    Aims based on mouse position, does not shoot. Basically a cannon

---
+++ Assets/Scripts/BallShoot.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallShoot : MonoBehaviour
+{
+    //this sscript is attached to the ball prefab to shoot the ball.
+    //It is controlled by the user's mouse input, where the user points to a location and clicks to shoot the ball in that direction.
+    //Peggle-style.
+    //swag
+
+    public float speed = 10.0f;
+    public Rigidbody rb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //track mouse position
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = 10;
+        Vector3 objectPos = Camera.main.WorldToScreenPoint(transform.position);
+        mousePos.x = mousePos.x - objectPos.x;
+        mousePos.y = mousePos.y - objectPos.y;
+
+        //calculate angle
+        float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+
+        //shoot ball
+        if (Input.GetMouseButtonDown(0))
+        {
+            rb.velocity = transform.right * speed;
+        }
+        
+    }
+}
