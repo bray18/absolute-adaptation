fea1a2df19875d9f9a70b3051a838e964121786a
KCSBach <112579594+KCSBach@users.noreply.github.com>
2024-03-09 11:06:58 -0600

    Tomato video works!!

--- Assets/Scripts/VideoPlayerScript.cs
+++ Assets/Scripts/VideoPlayerScript.cs
@@ -6,14 +6,23 @@ using UnityEngine.SceneManagement;
 public class VideoPlayerScript : MonoBehaviour
 {
     
-    
-    
     // Start is called before the first frame update
+    void EndReached(UnityEngine.Video.VideoPlayer vp) {
+        SceneManager.LoadScene("BasicLevel");
     
+    }
     void Start()
     {
-        GetComponent<UnityEngine.Video.VideoPlayer>().Play();
-        
+        GameObject camera = GameObject.Find("Main Camera");
+        var videoPlayer = camera.AddComponent<UnityEngine.Video.VideoPlayer>();
+        videoPlayer.url = "Assets/Videos/tomato anxiety.mp4";
+
+        // By default, VideoPlayers added to a camera will use the far plane.
+        // Let's target the near plane instead.
+        videoPlayer.renderMode = UnityEngine.Video.VideoRenderMode.CameraNearPlane;
+        videoPlayer.waitForFirstFrame = true;
+        videoPlayer.loopPointReached += EndReached;
+        videoPlayer.Play();
     }
 
     // Update is called once per frame
