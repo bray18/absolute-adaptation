0072ddbf62ca31f451e20af0e275659d0fe0591d
Brady Walters <49881821+bray18@users.noreply.github.com>
2024-03-09 12:01:27 -0600

    Gravity Goobers

---
+++ Assets/Scripts/ProjectileBehavior.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileBehavior : MonoBehaviour
+{
+    public float delayBeforeAffected = 1.0f; // Time in seconds before gravity affects the projectile
+    private bool isAffectedByGravity = false; // Initial state
+
+    void Start()
+    {
+        // Start the countdown to being affected by gravity
+        StartCoroutine(BecomeAffectedByGravityAfterDelay());
+    }
+
+    IEnumerator BecomeAffectedByGravityAfterDelay()
+    {
+        yield return new WaitForSeconds(delayBeforeAffected);
+        isAffectedByGravity = true;
+        gameObject.tag = "AffectedByGravity"; // Change the tag to make it affected by edge gravity
+    }
+
+    public bool IsAffectedByGravity()
+    {
+        return isAffectedByGravity;
+    }
+}
