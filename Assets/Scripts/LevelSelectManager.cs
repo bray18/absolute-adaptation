18cbe4e4b02625b2ac04a81c17ff33de8762665e
KCSBach <112579594+KCSBach@users.noreply.github.com>
2024-03-09 22:28:34 -0600

    fixed it to call the singleton stuff

--- Assets/Scripts/LevelSelectManager.cs
+++ Assets/Scripts/LevelSelectManager.cs
@@ -43,7 +43,7 @@ public class LevelSelectManager : MonoBehaviour
         if(!GameObject.FindWithTag("Music").GetComponent<AudioSource>().isPlaying)
         {
             audioSource = GameObject.FindWithTag("Music").GetComponent<AudioSource>();
-            audioSource.Play();
+            Singleton.Instance.GoAudio(audioSource);
         }
 
     }
